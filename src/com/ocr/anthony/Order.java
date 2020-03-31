a3fb8c05590e8d7a036f2c5ee45a079c2528753d
Chuppchpp <damien.grizon@gmail.com>
2020-03-31 20:39:10 +0200

    Mise en place d'un TP menu avec des tests

--- src/com/ocr/anthony/Order.java
+++ src/com/ocr/anthony/Order.java
@@ -1,10 +1,18 @@
 package com.ocr.anthony;
 
+import com.sun.source.doctree.SystemPropertyTree;
+
 public class Order {
     /**
      * Display all available menus in the restaurant.
      */
+
     public void displayAvailableMenu() {
+        System.out.println("Choix menu");
+        System.out.println("1 - poulet");
+        System.out.println("2 - boeuf");
+        System.out.println("3 - végétarien");
+        System.out.println("Que souhaitez-vous comme menu ?");
 
     }
     /**
@@ -12,6 +20,6 @@ public class Order {
      * @param nbMenu The selected menu.
      */
     public void displaySelectedMenu(int nbMenu) {
-
+        System.out.println("Vous avez choisi le menu " + nbMenu);
     }
 }
