514be0537520b081b51d5036fe2a324111a499ad
Chuppchpp <damien.grizon@gmail.com>
2020-04-01 13:21:26 +0200

    Changement des tests et ajouts des boucles if

--- src/com/ocr/anthony/Order.java
+++ src/com/ocr/anthony/Order.java
@@ -2,6 +2,8 @@ package com.ocr.anthony;
 
 import com.sun.source.doctree.SystemPropertyTree;
 
+import java.net.StandardSocketOptions;
+
 public class Order {
     /**
      * Display all available menus in the restaurant.
@@ -20,6 +22,14 @@ public class Order {
      * @param nbMenu The selected menu.
      */
     public void displaySelectedMenu(int nbMenu) {
-        System.out.println("Vous avez choisi le menu " + nbMenu);
+        if (nbMenu==1) {
+            System.out.println("Vous avez choisi comme menu : poulet");
+        }else if (nbMenu ==2) {
+            System.out.println("Vous avez choisi comme menu : boeuf");
+        }else if (nbMenu ==3){
+            System.out.println("Vous avez choisi comme menu : végétarien");
+        }else{
+            System.out.println("Vous n'avez pas choisi de menu parmi les choix proposés");
+        }
     }
 }
